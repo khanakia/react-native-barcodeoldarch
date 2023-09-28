492b5c3e9e913e1d5e0432efe6cad345e85e0677
khanakia <khanakia@gmail.com>
2023-09-28 17:28:52 +0530

    feat: added tap

--- ios/Barcodeoldarch.swift
+++ ios/Barcodeoldarch.swift
@@ -2,6 +2,7 @@ import Foundation
 import VisionKit
 import React
 import UIKit
+import AudioToolbox
 
 
 @objc(Barcodeoldarch)
@@ -9,16 +10,17 @@ class Barcodeoldarch: RCTEventEmitter, DataScannerViewControllerDelegate {
 
     @objc(multiply:withB:withResolver:withRejecter:)
         func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        sendEvent(withName: "onBarcodeScanned", body: ["test"])
         resolve(a*b)
     }
     
     let viewController = DataScannerViewController(
-      recognizedDataTypes: [.barcode()],
-      qualityLevel: .fast,
-      recognizesMultipleItems: false,
-      isHighFrameRateTrackingEnabled: false,
-      isHighlightingEnabled: true)
+        recognizedDataTypes: [.barcode()],
+        qualityLevel: .fast,
+        recognizesMultipleItems: false,
+        isHighFrameRateTrackingEnabled: false,
+        isPinchToZoomEnabled: true,
+        isGuidanceEnabled: true,
+        isHighlightingEnabled: true)
 
     var scannerAvailable: Bool {
       DataScannerViewController.isSupported && DataScannerViewController.isAvailable
@@ -41,20 +43,36 @@ class Barcodeoldarch: RCTEventEmitter, DataScannerViewControllerDelegate {
       }
     }
 
-    func dataScanner(_ dataScanner: DataScannerViewController, didAdd addedItems: [RecognizedItem], allItems: [RecognizedItem]) {
-        for item in addedItems {
-          switch item {
-             case .text(let text):
-               print("text: \(text.transcript)")
-             case .barcode(let barcode):
-                 print("barcode: \(barcode.payloadStringValue ?? "unknown")")
-                dataScanner.stopScanning()
-                dataScanner.dismiss(animated: true)
-                sendEvent(withName: "onBarcodeScanned", body: [barcode.payloadStringValue])
-             default:
-                 print("unexpected item")
-                sendEvent(withName: "onScanError", body: ["unable to scan"])
-           }
+//    func dataScanner(_ dataScanner: DataScannerViewController, didAdd addedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+//        for item in addedItems {
+//          switch item {
+//             case .text(let text):
+//               print("text: \(text.transcript)")
+//             case .barcode(let barcode):
+//                 print("barcode: \(barcode.payloadStringValue ?? "unknown")")
+////                dataScanner.stopScanning()
+////                dataScanner.dismiss(animated: true)
+//                sendEvent(withName: "onBarcodeScanned", body: [barcode.payloadStringValue])
+//             default:
+//                 print("unexpected item")
+//                sendEvent(withName: "onScanError", body: ["unable to scan"])
+//           }
+//        }
+//    }
+//
+    func dataScanner(_ dataScanner: DataScannerViewController, didTapOn item: RecognizedItem) {
+        switch item {
+        case .text(let text):
+            print("text: \(text.transcript)")
+        case .barcode(let barcode):
+            print("tap barcode: \(barcode.payloadStringValue ?? "unknown")")
+            dataScanner.stopScanning()
+            dataScanner.dismiss(animated: true)
+//            AudioServicesPlaySystemSound(1519)
+            AudioServicesPlaySystemSound(1520)
+            sendEvent(withName: "onBarcodeScanned", body: [barcode.payloadStringValue])
+        default:
+            print("unexpected item")
         }
     }
     
